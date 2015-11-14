a935ee5045ed4d675542a5e8869a6066763ee93d
Yoshua Wakeham <yoshwakeham@gmail.com>
2015-11-14 17:28:52 +1100

    Get simple hello world working in lanterna

--- src/main/java/com/github/yoshw/Snakey.java
+++ src/main/java/com/github/yoshw/Snakey.java
@@ -1,5 +1,10 @@
 package com.github.yoshw;
 
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
 /**
  * Snakey: a simple implementation of Snake.
  *
@@ -8,6 +13,22 @@ public class Snakey
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Terminal terminal = TerminalFacade.createTerminal(Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+        terminal.moveCursor(10, 5);
+        terminal.putCharacter('H');
+        terminal.putCharacter('e');
+        terminal.putCharacter('l');
+        terminal.putCharacter('l');
+        terminal.putCharacter('o');
+        terminal.putCharacter(' ');
+        terminal.putCharacter('W');
+        terminal.putCharacter('o');
+        terminal.putCharacter('r');
+        terminal.putCharacter('l');
+        terminal.putCharacter('d');
+        terminal.putCharacter('!');
+        terminal.moveCursor(10, 6);
+        terminal.flush();
     }
 }

03b92f62e9a2ea37c2e5ce7b87a05d8e079a81a2
Yoshua Wakeham <yoshwakeham@gmail.com>
2015-11-15 18:26:51 +1100

    Create stub Snake and Segment classes

---
+++ src/main/java/com/github/yoshw/snakey/Segment.java
@@ -0,0 +1,10 @@
+package com.github.yoshw.snakey;
+
+/**
+ * Created by Yosh on 15/11/2015.
+ */
+public class Segment extends GameObject {
+    public Segment(Cell loc) {
+        super(loc);
+    }
+}
---
+++ src/main/java/com/github/yoshw/snakey/Snake.java
@@ -0,0 +1,9 @@
+package com.github.yoshw.snakey;
+
+/**
+ * Created by Yosh on 15/11/2015.
+ */
+public class Snake {
+    public Snake() {
+    }
+}
